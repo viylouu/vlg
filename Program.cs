b78a462b90feb6bef2742d987e4fff5ea0aa88fa
viylouu <honeefunnee@gmail.com>
2023-12-22 02:59:24 -0700

    added some dotnet stuff

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
-﻿using SimulationFramework;
+﻿using ImGuiNET;
+using SimulationFramework;
 using SimulationFramework.Desktop;
 using SimulationFramework.Drawing;
 using SimulationFramework.Input;
 using System.Numerics;
-using vlg;
 
 Simulation sim = Simulation.Create(Init, Rend);
 sim.Run(new DesktopPlatform());
@@ -11,10 +11,12 @@ sim.Run(new DesktopPlatform());
 partial class Program {
     static int yscroll = 0;
     static float smoothyscroll = 0;
+    static int yscrollACT = 0;
+    static float smoothyscrollACT = 0;
     static readonly float smooth = 5;
 
     static ITexture[] gameimages = new ITexture[] { Graphics.LoadTexture(@"Assets\Menu\sandy.png") };
-    static readonly float tilesmooth = 15;
+    static readonly float tilesmooth = 25;
 
     static ISound ambience = Audio.LoadSound(@"Assets\Menu\ambience.wav");
     static SoundPlayback ambientPlayback = null;
@@ -26,14 +28,22 @@ partial class Program {
     static int stscroll = 0;
 
     static float menutitley = 0;
+    static float menutitleyACT = 0;
+
+    static bool hasUsername = false;
+
+    static Vector2 windowdims = Vector2.One;
 
     static void Init() {
         Audio.MasterVolume = 0.0125f;
+
+        windowdims = new Vector2(ImGui.GetMainViewport().Size.X, ImGui.GetMainViewport().Size.Y);
 
         Window.Title = "vlg";
         Simulation.SetFixedResolution(1920, 1080, Color.Black, false, false, false);
 
         menutitley = -1080 * 2;
+        menutitleyACT = -windowdims.Y * 2;
 
         ambientPlayback = ambience.Play();
     }
@@ -41,6 +51,7 @@ partial class Program {
     static void Rend(ICanvas canv) {
         drawBG(canv);
         drawGames(canv);
+        drawOther(canv);
 
         audio();
 
@@ -63,8 +74,25 @@ partial class Program {
     static void drawGames(ICanvas canv) {
         canv.Translate(new Vector2(canv.Width / 2f - 128, canv.Height + canv.Height / 2f - smoothyscroll - 128));
         canv.Fill(gameimages[0]);
-        canv.DrawRoundedRect(new Vector2(128, 128), Vector2.One * 256, 10, Alignment.Center);
+        canv.DrawRoundedRect(new Vector2(128, 128), Vector2.One * 256, tilesmooth, Alignment.Center);
         canv.ResetState();
+    }
+
+    static void drawOther(ICanvas canv) {
+        canv.Fill(Color.White);
+        canv.FontSize(24);
+        canv.DrawText("welcome to vlg", new Vector2(canv.Width / 2, menutitley - smoothyscroll), Alignment.Center);
+
+        if (!hasUsername) {
+            ImGui.Begin("insert username");
+
+            ImGui.SetWindowSize(new Vector2(336, 140));
+            ImGui.SetWindowPos(new Vector2(windowdims.X / 2 - 336 / 2f, (menutitleyACT - windowdims.Y / 2 + 120) - smoothyscrollACT));
+
+            ImGui.Text("please insert username below");
+
+            ImGui.End();
+        }
     }
 
     static void updVars() {
@@ -73,7 +101,16 @@ partial class Program {
         yscroll += -(int)Mouse.ScrollWheelDelta * 36;
         yscroll = Math.Clamp(yscroll, 0, canv.Height);
 
+        yscrollACT += -(int)(Mouse.ScrollWheelDelta * (windowdims.Y / 30));
+        yscrollACT = (int)Math.Clamp(yscrollACT, 0, windowdims.Y);
+
         smoothyscroll += (yscroll - smoothyscroll) / (smooth * (1 / (Time.DeltaTime * 30)));
+        smoothyscrollACT += (yscrollACT - smoothyscrollACT) / (smooth * (1 / (Time.DeltaTime * 30)));
+
+        menutitley += (canv.Height / 2f - menutitley) / (smooth * 1.25f * (1 / (Time.DeltaTime * 30)));
+        menutitleyACT += (windowdims.X / 2f - menutitleyACT) / (smooth * 1.25f * (1 / (Time.DeltaTime * 30)));
+
+        windowdims = new Vector2(ImGui.GetMainViewport().Size.X, ImGui.GetMainViewport().Size.Y);
     }
 
     static void drawBG(ICanvas canv) {
